d4bbaa358bbdbe33e4555bb974d66b15710e42e9
CandidoAg <candido.aguilar.cano@gmail.com>
2026-06-24 14:04:59 +0200

    fix(client): remove unused createContext and useContext imports to fix build

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createContext, useContext } from 'react';
+import React, { useState, useEffect} from 'react';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import { AuthPage } from './pages/AuthPage';
 import { MainLayout } from './layouts/MainLayout';
